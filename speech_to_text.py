d1aa463e59cb5bf2d0ebc5c945299ba0297e3881
Ed Summers <ehs@pobox.com>
2024-12-04 14:47:51 -0500

    reformatted

--- speech_to_text.py
+++ speech_to_text.py
@@ -23,7 +23,11 @@ from whisper.utils import get_writer
 from mypy_boto3_s3.service_resource import Bucket, S3ServiceResource
 from mypy_boto3_sqs.service_resource import SQSServiceResource, Queue
 
-honeybadger.configure(api_key=os.environ.get("HONEYBADGER_API_KEY", ""), environment=os.environ.get("HONEYBADGER_ENV", "stage"))
+honeybadger.configure(
+    api_key=os.environ.get("HONEYBADGER_API_KEY", ""),
+    environment=os.environ.get("HONEYBADGER_ENV", "stage"),
+)
+
 
 def main(daemon=True) -> None:
     # loop forever looking for jobs unless daemon option says not to
@@ -264,7 +268,9 @@ def report_error(message: str, job: Optional[Dict], traceback: str) -> None:
     """
     full_message = message + "\n" + traceback
     logging.exception(full_message)
-    honeybadger.notify("Whisper AWS process \n" + message, context={"job": job, "traceback": traceback})
+    honeybadger.notify(
+        "Whisper AWS process \n" + message, context={"job": job, "traceback": traceback}
+    )
 
     # it's possible that we are reporting an error without a job
     # we can only send a message to the DONE queue if we have a job!
